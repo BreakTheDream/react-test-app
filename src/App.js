bfeb00a9f053d6ba6b1ce171593eac68d8ba20d6
Break <nikita.antipkin2014@yandex.ru>
2019-01-22 18:04:41 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import logo from './logo.svg';
+import './App.css';
+import List from './List';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// class GroceryList extends Component {
+//   render() {
+//     return (
+//       <ul>
+//         <ListItem quantity="1" name="Bread" />
+//         <ListItem quantity="6" name="Eggs" />
+//         <ListItem quantity="2" name="Milk" />
+//       </ul>
+//     )
+//   }
+// }
+
+// class ListItem extends Component {
+//   render() {
+//     return(
+//       <li>
+//         {this.props.quantity} * {this.props.name}
+//       </li>
+//     )
+//   }
+// }
+
+// export default GroceryList;
+
+// export default App;
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <List id='todo' title="To Do" cards={this.props.cards.filter((card) => card.status === "todo")} />
+                <List id='in-progress' title="In Progress" cards={this.props.cards.filter((card) => card.status === "in-progress")} />
+                <List id='done' title="Done" cards={this.props.cards.filter((card) => card.status === "done")} />
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    cards: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+let cardsList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        description: "I should read the **whole** book",
+        color: "#BD8D31",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
+        color: "#3A7E28",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    }
+];
+
+ReactDOM.render(<App cards={cardsList} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
